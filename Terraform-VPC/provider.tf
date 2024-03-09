d5c17bf2735f22b32deddb15986bf7a3be5790b0
PanGian97 <pangianmain@gmail.com>
2024-03-09 02:52:14 +0200

    final-final-commit

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -2,17 +2,17 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.16.1"
+      version = "5.40.0"
     }
   }
 
   backend "s3" {
     bucket = "terraform-remote-backend-s3"
     key    = "dev/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-central-1"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
